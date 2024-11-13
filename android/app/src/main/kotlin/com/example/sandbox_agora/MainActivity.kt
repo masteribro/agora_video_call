279b228cc620676738773b51f5e13440d22a9e67
Masteribrahim1 <mohammed.ibrahim@dreamlabs.com.ng>
2024-11-13 05:05:25 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/sandbox_agora/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sandbox_agora
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/constant/constant.dart
@@ -0,0 +1,6 @@
+// Fill in the app ID obtained from the Agora console
+const appId = "dabbbc48734a416f912b98f7132a7fc6";
+// Fill in the temporary token generated from Agora Console
+const token = "007eJxTYBBUq+eZYbb46D5H6yC+OS2MMTy/d9cJct9m/PlqdTWzb7ECQ0piUlJSsomFubFJoomhWZqloVGSpUWauaGxUaJ5WrJZxweD9IZARoZe/+PMjAwQCOKzMJSkFpcwMAAAgzMdaA==";
+// Fill in the channel name you used to generate the token
+const channel = "test";
